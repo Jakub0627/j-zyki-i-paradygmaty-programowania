60dfbb4fbc019d775eab95d6d5565dee037f2975
Jakub <kuba.gladysz@hotmail.com>
2024-11-10 17:38:55 +0100

    Finished lab 1
    Dokończone zostały wszystkie zadania z lab1

--- PythonZadania/python/lab1/zad4.py
+++ PythonZadania/python/lab1/zad4.py
@@ -13,3 +13,87 @@ oraz mapowanie.
 • Program powinien zwracać maksymalną wartość oraz listę przedmiotów, które powinny zostać
 wybrane do plecaka.
 '''
+
+#######################################################################################################################
+
+print("Podejście Proceduralne: \n")
+
+
+def knapsack_dynamic(items, capacity):
+    # Tworzymy tablicę dp, która przechowuje maksymalną wartość dla każdej pojemności plecaka
+    dp = [[0] * (capacity + 1) for _ in range(len(items) + 1)]
+
+    # Przechodzimy przez wszystkie przedmioty i pojemności plecaka
+    for i in range(1, len(items) + 1):
+        for w in range(capacity + 1):
+            # Jeśli przedmiot się zmieści w plecaku
+            if items[i - 1][0] <= w:
+                # Wybieramy większą wartość: nie bierzemy przedmiotu lub bierzemy go
+                dp[i][w] = max(dp[i - 1][w], dp[i - 1][w - items[i - 1][0]] + items[i - 1][1])
+            else:
+                dp[i][w] = dp[i - 1][w]
+
+    # Odtwarzamy przedmioty, które zostały wybrane do plecaka
+    selected_items = []
+    w = capacity
+    for i in range(len(items), 0, -1):
+        if dp[i][w] != dp[i - 1][w]:
+            selected_items.append(items[i - 1])
+            w -= items[i - 1][0]
+
+    return dp[len(items)][capacity], selected_items
+
+
+# Przykładowe dane
+items = [(2, 3), (3, 4), (4, 5), (5, 8)]  # (waga, wartość)
+capacity = 5
+
+max_value, selected_items = knapsack_dynamic(items, capacity)
+print("Maksymalna wartość:", max_value)
+print("Wybrane przedmioty:", selected_items)
+
+#######################################################################################################################
+
+print("\n\nPodejście Funkcyjne: \n")
+
+
+def knapsack_recursive(items, capacity, idx=0, memo=None):
+    # Inicjalizujemy memoization
+    if memo is None:
+        memo = {}
+
+    # Sprawdzamy, czy wynik dla tego stanu (idx, capacity) jest już obliczony
+    if (idx, capacity) in memo:
+        return memo[(idx, capacity)]
+
+    # Jeśli nie ma więcej przedmiotów lub pojemność plecaka jest 0, zwracamy 0
+    if idx == len(items) or capacity == 0:
+        return 0, []
+
+    # Jeśli przedmiot nie zmieści się w plecaku
+    if items[idx][0] > capacity:
+        return knapsack_recursive(items, capacity, idx + 1, memo)
+
+    # Rekurencyjne wywołania: wybieramy lub nie wybieramy przedmiot
+    value_without, items_without = knapsack_recursive(items, capacity, idx + 1, memo)
+    value_with, items_with = knapsack_recursive(items, capacity - items[idx][0], idx + 1, memo)
+
+    value_with += items[idx][1]
+    items_with = [items[idx]] + items_with
+
+    # Zapisujemy wynik w memo
+    if value_without > value_with:
+        memo[(idx, capacity)] = value_without, items_without
+        return value_without, items_without
+    else:
+        memo[(idx, capacity)] = value_with, items_with
+        return value_with, items_with
+
+# Przykładowe dane
+items = [(2, 3), (3, 4), (4, 5), (5, 8)]  # (waga, wartość)
+capacity = 5
+
+max_value, selected_items = knapsack_recursive(items, capacity)
+print("Maksymalna wartość:", max_value)
+print("Wybrane przedmioty:", selected_items)
+
--- PythonZadania/python/lab1/zad5.py
+++ PythonZadania/python/lab1/zad5.py
@@ -14,3 +14,98 @@ przefiltrować i posortować zadania oraz wybrać optymalny harmonogram.
 • Program powinien zwracać maksymalną możliwą nagrodę i listę zadań w optymalnym
 harmonogramie.
 '''
+
+#######################################################################################################################
+
+print("\nRozwiązanie proceduralne: \n")
+
+class Task:
+    def __init__(self, start, end, reward):
+        self.start = start
+        self.end = end
+        self.reward = reward
+
+    def __repr__(self):
+        return f"Task({self.start}, {self.end}, {self.reward})"
+
+
+def greedy_task_selection(tasks):
+    # Sortujemy zadania według czasu zakończenia
+    tasks.sort(key=lambda task: task.end)
+
+    # Inicjalizujemy zmienne
+    last_end_time = 0
+    total_reward = 0
+    selected_tasks = []
+
+    # Wybieramy zadania, które nie kolidują
+    for task in tasks:
+        if task.start >= last_end_time:
+            selected_tasks.append(task)
+            total_reward += task.reward
+            last_end_time = task.end
+
+    return total_reward, selected_tasks
+
+
+# Przykładowe zadania
+tasks = [
+    Task(1, 4, 10),
+    Task(2, 6, 5),
+    Task(5, 7, 8),
+    Task(8, 9, 12),
+    Task(3, 5, 7),
+]
+
+total_reward, selected_tasks = greedy_task_selection(tasks)
+print("Max Reward:", total_reward)
+print("Selected Tasks:", selected_tasks)
+
+#######################################################################################################################
+
+print("\n\nPodejście Funkcyjne: \n")
+
+from functools import reduce
+
+
+class Task:
+    def __init__(self, start, end, reward):
+        self.start = start
+        self.end = end
+        self.reward = reward
+
+    def __repr__(self):
+        return f"Task({self.start}, {self.end}, {self.reward})"
+
+
+def functional_task_selection(tasks):
+    # Sortujemy zadania po czasie zakończenia
+    sorted_tasks = sorted(tasks, key=lambda task: task.end)
+
+    # Funkcja do wyboru zadań, które nie kolidują
+    def select_task(acc, task):
+        if acc[-1].end <= task.start:  # Sprawdzamy, czy zadanie nie koliduje
+            acc.append(task)
+        return acc
+
+    # Pierwszym zadaniem jest zawsze to, które kończy się najwcześniej
+    selected_tasks = reduce(select_task, sorted_tasks, [Task(0, 0, 0)])
+
+    # Obliczamy łączną nagrodę
+    total_reward = sum(task.reward for task in selected_tasks[1:])  # Pomijamy dummy task
+
+    return total_reward, selected_tasks[1:]
+
+
+# Przykładowe zadania
+tasks = [
+    Task(1, 4, 10),
+    Task(2, 6, 5),
+    Task(5, 7, 8),
+    Task(8, 9, 12),
+    Task(3, 5, 7),
+]
+
+total_reward, selected_tasks = functional_task_selection(tasks)
+print("Max Reward:", total_reward)
+print("Selected Tasks:", selected_tasks)
